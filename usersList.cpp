faee02f9c153cfbd07ae84a4c206a4e653ba58fb
irksome0 <125121047+irksome0@users.noreply.github.com>
2023-06-04 12:11:34 +0300

    Add files via upload

---
+++ usersList.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "usersList.h"
+#if __has_include("usersList.g.cpp")
+#include "usersList.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::LoginSystem::implementation
+{
+    int32_t usersList::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void usersList::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void usersList::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        Button().Content(box_value(L"Clicked"));
+    }
+}
